4a1156771576d4c9cd10b1efd50f096e05f6185d
Mas0w <masow2003@gmail.com>
2025-03-15 21:42:50 -0500

    Enemy melee animation and flip sprite

--- Assets/Scripts/Enemies/EnemyMele.cs
+++ Assets/Scripts/Enemies/EnemyMele.cs
@@ -4,37 +4,43 @@ public class EnemyMele : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private int timeDirect;
+    [SerializeField] private float life;
+    [SerializeField] private float maxLife;
     private float timeNextDirect;
-    bool isMovingLeft = true;
+    bool isMovingLeft = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
-
-    // Update is called once per frame
-    
-    
-
-    void FixedUpdate()
+    private void Awake()
     {
-        timeNextDirect += Time.deltaTime;
+        life = maxLife;
+    }
+    public void TakeDamage(float damage)
+    {
+        life -= damage;
 
-        if (timeNextDirect >= timeDirect)
-        {
-            isMovingLeft = !isMovingLeft;
-            timeNextDirect = 0;
-        }
-
-        if (isMovingLeft)
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        else
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
     }
 
+void FixedUpdate()
+{
+    timeNextDirect += Time.fixedDeltaTime;
+
+    if (timeNextDirect >= timeDirect)
+    {
+        isMovingLeft = !isMovingLeft;
+        timeNextDirect = 0;
+        transform.localRotation = isMovingLeft ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
+    }
+
+    if (isMovingLeft)
+    {
+        transform.Translate(Vector3.left * speed * Time.fixedDeltaTime, Space.World);
+    }
+    else
+    {
+        transform.Translate(Vector3.right * speed * Time.fixedDeltaTime, Space.World);
+    }
+}
 }
