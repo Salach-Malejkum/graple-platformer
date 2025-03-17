50521b32fb9e98522828bed22e580c42eb55a911
Mateusz Salach <57152223+Salach-Malejkum@users.noreply.github.com>
2025-03-17 13:45:46 +0100

    Add basic player movement, animations and input system, rename some functions

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,36 +1,96 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MainCharacter : MonoBehaviour
 {
+    [Header("Player properties")]
     [SerializeField] private float life;
     [SerializeField] private float maxLife;
     [SerializeField] private float damage;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float speed;
+    
+
+    [Header("Sprite and animations properties")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private bool isGrounded = true;
+    private float moveInput;
+    private Rigidbody2D rb2d;
+
+
     private void Awake()
     {
         life = maxLife;
+        rb2d = GetComponent<Rigidbody2D>();
     }
     public void TakeDamage(float damage)
     {
         life -= damage;
 
+        if (life <= 0)
+        {
+            Die();
+        }
     }
 
     void FixedUpdate()
     {
-        if (life <= 0)
+        rb2d.linearVelocityX = moveInput * speed;
+        print(rb2d.linearVelocityY);
+
+        if (!isGrounded)
         {
-            Diying();
+            animator.SetFloat("YVelocity", rb2d.linearVelocityY);
         }
     }
-    void Diying()
+
+    void Die()
     {
         Destroy(gameObject);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        isGrounded = true;
         if (collision.GetComponent<EnemyMele>())
         {
             collision.transform.GetComponent<EnemyMele>().TakeDamage(damage);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+
+    private void OnMovement(InputValue input)
+    {   
+        moveInput = input.Get<float>();
+        
+        animator.SetBool("IsRunning", moveInput != 0);
+        FlipSprite();
+    }
+
+    private void OnJump()
+    {
+        if (isGrounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
+        }
+    }
+
+    private void FlipSprite()
+    {
+        if (moveInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput < 0)
+        {
+            spriteRenderer.flipX = true;
         }
     }
 }
